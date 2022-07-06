d22de108847735e758b70188d37f9cab0b24ee38
ntabucejo <ntabucejo@gmail.com>
2022-07-07 00:41:57 +0800

    Update page sample showcase

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,24 +2,7 @@ import type { NextPage } from "next";
 import Layout from "../../components/layouts";
 
 const Dashboard: NextPage = () => {
-  return (
-    <Layout title="Dashboard">
-      <section className="flex h-full w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a href="https://nextjs.org" className="text-primary-accent">
-            Dashboard
-          </a>
-        </h1>
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.js
-          </code>
-        </p>
-      </section>
-    </Layout>
-  );
+  return <Layout title="Dashboard">{/* Contents */}</Layout>;
 };
 
 export default Dashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,29 +6,44 @@ import { CashIcon } from "@heroicons/react/solid";
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
-      <section className="flex h-full w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <section className="mt-52  flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
           <a href="https://nextjs.org" className="text-primary-accent">
             Footsprint
           </a>
         </h1>
-        <p className="mt-3 text-2xl">
+        <p className="mt-3 mb-24 text-2xl">
           Get started by editing{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.js
           </code>
         </p>
+
         <Button
-          onClick={() => {}}
+          Icon={CashIcon}
+          onClick={() => {
+            console.log("Clicked Buy");
+          }}
           dialogbox={{
             Icon: CashIcon,
-            title: "Payment",
-            message: "Please confirm your payment",
-            action: {
-              name: "Confirm",
-              onClick: () => {},
-            },
+            title: "Buy",
+            message: "Are you sure you want to buy this item?",
+            buttons: [
+              {
+                text: "Cancel",
+                variant: "transparent",
+                onClick: () => {
+                  console.log("Cancelled");
+                },
+              },
+              {
+                text: "Yes",
+                onClick: () => {
+                  console.log("Success");
+                },
+              },
+            ],
           }}>
           Buy
         </Button>
